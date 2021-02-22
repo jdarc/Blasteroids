7bca764c36180c54cc63a4bce8cd35d897d25f44
Jean d'Arc <darc.void@googlemail.com>
2021-02-22 21:19:42 +0000

    Added copyright statement to all sources

--- src/main/kotlin/game/GunNode.kt
+++ src/main/kotlin/game/GunNode.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Jean d'Arc.
+ * Copyright (c) 2021 Jean d'Arc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -16,6 +16,7 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package game
 
 import engine.core.Color
